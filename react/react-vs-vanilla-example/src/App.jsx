85711f31914282faf6c79b766aeaa0a3e7d8c86c
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2024-02-15 09:42:48 -0300

    feat: finish first react app

--- react/react-vs-vanilla-example/src/App.jsx
+++ react/react-vs-vanilla-example/src/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering"
+  ]
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
 
   return (
-    <>
+    <div>
+    <header>
+      <img src="react.svg" alt="React logo" />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>React.js</h1>
+        <p>i.e., using the React library for rendering the UI</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    </header>
+
+    <div id="tabs">
+      <menu>
+        <button
+          className={activeContentIndex === 0 ? "active" : ""}
+          onClick={() => setActiveContentIndex(0)}
+        >
+          Why React?
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          className={activeContentIndex === 1 ? "active" : ""}
+          onClick={() => setActiveContentIndex(1)}
+        >
+          Core Features
+        </button>
+      </menu>
+      <div id="tab-content">
+        <ul>
+          {content[activeContentIndex].map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  </div>
   )
 }
 
