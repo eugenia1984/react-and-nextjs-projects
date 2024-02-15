4cffcd19c22fd4b72b80724a9656bfc0ce440ded
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2024-02-15 18:56:06 -0300

    feat: post list

--- react/post-app/src/components/PostList.jsx
+++ react/post-app/src/components/PostList.jsx
@@ -12,7 +12,7 @@ const PostsList = ({ isPosting, onStopPosting }) => {
   const [posts, setPosts] = useState([]);
 
   const addPostHandler = (postData) => {
-    setPosts((existingPosts) => [postData, ...existingPosts])
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   };
 
   return (
@@ -22,9 +22,24 @@ const PostsList = ({ isPosting, onStopPosting }) => {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author="Eugenia" body="Check out the full course!" />
-      </ul>
+
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post, index) => (
+            <Post
+              key={post.author + index}
+              author={post.author}
+              body={post.body}
+            />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 };
